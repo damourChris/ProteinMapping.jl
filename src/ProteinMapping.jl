cdf1d22a325882fb0e94bffa489fee8a2733e9c1
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-09-21 22:20:53 +0200

    feat: add function to map expression data to stable Ensembl peptide IDs

--- src/ProteinMapping.jl
+++ src/ProteinMapping.jl
@@ -1,5 +1,37 @@
 module ProteinMapping
 
-# Write your package code here.
+include("resolve_env.jl")
+
+using ExpressionData
+using DataFrames
+using RCall
+
+function map_to_stable_ensembl_peptide(eset::ExpressionSet, attribute::String;
+                                       gene_col::String="ensembl_id",
+                                       mart_id::String="ensembl",
+                                       mart_dataset::String="hsapiens_gene_ensembl")
+    @rput eset gene_col mart_id mart_dataset attribute r_utils_path
+
+    R"""
+    suppressPackageStartupMessages({
+        library(biomaRt)
+        
+        library(httr)
+    })
+    set_config(config(ssl_verifypeer = 0L))
+    mart <- useMart(mart_id, dataset = mart_dataset)
+
+    mapping <- 
+    getBM(attributes = c("ensembl_peptide_id", attribute),
+          filters = gene_col,
+          values = unique(assayData(eset)[[gene_col]]),
+          mart = mart)
+    """
+
+    mapping_r = @rget mapping
+    mapping = convert(DataFrame, mapping_r)
+
+    return esetmapping
+end
 
 end
