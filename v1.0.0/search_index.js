76a81ddd0874527dca3f6875a35690dc23f616fb
Documenter.jl <documenter@juliadocs.github.io>
2024-09-21 21:31:14 +0000

    build based on 68df1ea

---
+++ v1.0.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ProteinMapping","category":"page"},{"location":"#ProteinMapping","page":"Home","title":"ProteinMapping","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProteinMapping.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ProteinMapping]","category":"page"}]
+}
